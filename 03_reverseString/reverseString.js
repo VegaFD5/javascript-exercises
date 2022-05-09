8398f8f626e3fccc5a88f0e538fb881001ec033c
VegaFD5 <FD5honeydew@gmail.com>
2022-05-09 12:45:05 +1200

    Create loop to fill empty string in reverse

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -2,6 +2,11 @@ const reverseString = function(word) {
     let arrString= [];
     arrString=word.split("");
     let revArray = [];
+    for (let i = arrString.length-1; i>=0; i--){
+        revArray+= arrString[i];
+       
+   }
+   return revArray.toString();
     
 
 
