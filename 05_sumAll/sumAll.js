4071b6e5bd7663f1faa260dbf30536b0664057bd
VegaFD5 <FD5honeydew@gmail.com>
2022-05-14 21:04:45 +1200

    Add complete sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if(a<0 || b<0){
+        return "ERROR";
+      }
+
+      if(typeof(a)!=="number" || typeof(b)!=="number"){
+        return "ERROR";
+      }
+
+      if(a>b){
+        let temp = b; 
+        b= a;
+        a = temp;
+      }
+
+      let sum =0;
+      for(i=a; i<=b; i++){
+        sum = sum+ i;
+      }
+      return sum;
 
 };
 
