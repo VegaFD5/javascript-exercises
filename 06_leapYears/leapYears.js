3c1ccae36c9bab92b8cb397b8ea6cfd33031fd44
VegaFD5 <FD5honeydew@gmail.com>
2022-05-15 18:11:16 +1200

    Replace console.log returns with boolean

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,10 +1,9 @@
 const leapYears = function(year) {
-    if(year%4 == 0 && year%100 !=0){
-        console.log("true");
-    }else{
-        console.log(false);
-    }
-
+    if((year%4 == 0 && year%100 !=0)|| (year%400 ==0)){
+        return true;
+      }else{
+        return false;
+      }
 };
 
 // Do not edit below this line
